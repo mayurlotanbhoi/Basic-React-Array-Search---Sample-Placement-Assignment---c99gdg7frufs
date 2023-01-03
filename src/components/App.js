1a2c4afd7a37524951e653dab4fa2fda537b4e8c
mayurlotanbhoi <mayurbhoi200@gmail.com>
2023-01-03 09:51:53 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
 
-import '../styles/App.css';
+import "../styles/App.css";
 
+const searchArray = [
+  "Newton",
+  "School",
+  "Newton School",
+  "React",
+  "Preact",
+  "Node",
+  "Mongoose",
+  "Angular",
+  "Vue",
+  "Apple",
+  "Microsoft",
+  "Netflix",
+  "Meta",
+];
 
-const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
+  const [sortedarray, setsortedarray] = useState([...searchArray]);
 
-  
+  const sortarra = (e) => {
+    console.log(e.target.value);
+
+    const sortedarray1 = sortedarray.filter((item) => {
+      // console.log(item);
+      return item.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+
+    if (e.target.value == "") {
+      setsortedarray([...searchArray]);
+    } else {
+      setsortedarray([...sortedarray1]);
+    }
+  };
   return (
     <div id="main">
-      
+      <input onChange={(e) => sortarra(e)} />
+      <p>result</p>
+      <ul>
+        {sortedarray.map((items) => {
+          return (
+            <>
+              <li>{items}</li>
+            </>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
